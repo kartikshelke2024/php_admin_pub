72dabb0f318dc6c59d615df8ffa89dee012e4a73
Kartik Shelke <kartikstrides@gmail.com>
2024-08-15 22:56:46 +0530

    Update .gitignore to ignore specific files and directories

--- admin/config/base_path.php
+++
@@ -1,4 +0,0 @@
-<?php
-$base_path = "http://localhost/client_data/02_sunglidelubricants.com/";
-// $base_path = "https://sunglidelubricants.com/demo2/";
-?>
--- admin/logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-session_destroy();
-//session_unset(); // unset all the sessions
-echo '<script type="text/javascript">window,location = "login";</script>';
-?>
